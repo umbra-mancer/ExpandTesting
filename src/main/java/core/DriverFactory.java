34c4f0fd53ff15bff94b308deccbdc6d06cf29b1
umbra-mancer <darklyfalse@tutamail.com>
2026-01-03 18:59:24 +0530

    Add headless support for CI in DriverFactory

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -3,13 +3,31 @@ package core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     private static WebDriver driver;
 
+
     public static WebDriver getDriver(){
         if(driver == null){
+            ChromeOptions options = new ChromeOptions();
+            String headless = System.getProperty("headless", "false");
+
+            if (headless.equalsIgnoreCase("true")) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            if (headless.equalsIgnoreCase("true")) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            }
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
             driver.manage().window().maximize();
